28c2b77ac665828e47a3c48c01f960516168069e
Mohamedsayhii <mohamedsaihii@outlook.fr>
2024-12-07 23:13:33 +0100

    Set up the app router

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const path = require('node:path');
+const passport = require('passport');
+const session = require('express-session');
+const { PrismaSessionStore } = require('@quixo3/prisma-session-store');
+const { PrismaClient } = require('@prisma/client');
+
+require('dotenv').config();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(
+	session({
+		cookie: {
+			maxAge: 7 * 24 * 60 * 60 * 1000, // ms
+		},
+		secret: 'a santa at nasa',
+		resave: true,
+		saveUninitialized: true,
+		store: new PrismaSessionStore(new PrismaClient(), {
+			checkPeriod: 2 * 60 * 1000, //ms
+			dbRecordIdIsSessionId: true,
+			dbRecordIdFunction: undefined,
+		}),
+	})
+);
+
+require('./config/passport');
+app.use(passport.session());
+
+app.listen(3000, console.log('express server is listening on port 3000'));
