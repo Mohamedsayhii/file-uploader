2b12dd3c38bcb9fcb45b622316e1401fd9135124
Mohamedsayhii <mohamedsaihii@outlook.fr>
2024-12-13 22:56:49 +0100

    Create modal for the files and folders forms

--- controllers/homepageController.js
+++ controllers/homepageController.js
@@ -2,6 +2,14 @@ exports.getHomepage = (req, res) => {
 	res.render('homepage');
 };
 
+exports.getCreateFolderForm = (req, res) => {
+	res.render('homepage', { showModal: 'folder' });
+};
+
+exports.getUploadFileForm = (req, res) => {
+	res.render('homepage', { showModal: 'file' });
+};
+
 exports.getLogout = (req, res, next) => {
 	req.logout((err) => {
 		if (err) {
--- routers/homepageRouter.js
+++ routers/homepageRouter.js
@@ -1,9 +1,18 @@
 const { Router } = require('express');
-const { getHomepage, getLogout } = require('../controllers/homepageController');
+const {
+	getHomepage,
+	getLogout,
+	getUploadFileForm,
+	getCreateFolderForm,
+} = require('../controllers/homepageController');
 
 const homepageRouter = Router();
 
 homepageRouter.get('/', getHomepage);
+homepageRouter.get('/createfolder', getCreateFolderForm);
+// homepageRouter.post('/createfolder', postCreateFolderForm);
+homepageRouter.get('/uploadfile', getUploadFileForm);
+// homepageRouter.post('/uploadfile', postUploadFileForm);
 homepageRouter.get('/logout', getLogout);
 
 module.exports = homepageRouter;
