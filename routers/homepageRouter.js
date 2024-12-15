6ca11cf91305a0bff929e9865d41d918ba0db021
Mohamedsayhii <mohamedsaihii@outlook.fr>
2024-12-15 21:22:03 +0100

    Integrate the multer middleware

--- routers/homepageRouter.js
+++ routers/homepageRouter.js
@@ -7,15 +7,34 @@ const {
 	postCreateFolderForm,
 	postDeleteFolder,
 } = require('../controllers/homepageController');
+const multer = require('multer');
 
 const homepageRouter = Router();
+
+const storage = multer.diskStorage({
+	destination: function (req, file, cb) {
+		cb(null, 'uploads');
+	},
+	filename: function (req, file, cb) {
+		cb(null, file.originalname);
+	},
+});
+
+const upload = multer({ storage });
 
 homepageRouter.get('/', getHomepage);
 homepageRouter.get('/createfolder', getCreateFolderForm);
 homepageRouter.post('/createfolder', postCreateFolderForm);
 homepageRouter.post('/:foldername/delete', postDeleteFolder);
 homepageRouter.get('/uploadfile', getUploadFileForm);
-// homepageRouter.post('/uploadfile', postUploadFileForm);
+homepageRouter.post(
+	'/uploadfile',
+	upload.single('uploaded_file'),
+	function (req, res) {
+		console.log(req.file, req.body);
+		res.redirect('/home');
+	}
+);
 homepageRouter.get('/logout', getLogout);
 
 module.exports = homepageRouter;
