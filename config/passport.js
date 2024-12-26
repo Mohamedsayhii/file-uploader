2adbdab2aab630f332b747535c81fd16453b8b18
Mohamedsayhii <mohamedsaihii@outlook.fr>
2024-12-26 12:38:02 +0100

    Modify passport to use prisma directly

--- config/passport.js
+++ config/passport.js
@@ -1,12 +1,19 @@
 const bcrypt = require('bcryptjs');
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
-const db = require('../database/queries');
+const { PrismaClient } = require('@prisma/client');
+
+const prisma = new PrismaClient();
 
 passport.use(
 	new LocalStrategy(async (username, password, done) => {
 		try {
-			const user = await db.getUser(username);
+			const user = await prisma.user.findFirst({
+				where: {
+					username,
+				},
+			});
+
 			if (!user) {
 				return done(null, false, { message: 'Incorrect Username' });
 			}
@@ -27,7 +34,7 @@ passport.serializeUser((user, done) => done(null, user.id));
 
 passport.deserializeUser(async (id, done) => {
 	try {
-		const user = await db.getUserById(id);
+		const user = await prisma.user.findFirst({ where: { id } });
 		done(null, user);
 	} catch (err) {
 		done(err);
